4724af2847ae55915fcd2fad8d10beef49137c66
Daylen1160 <daylen924@gmail.com>
2026-03-14 14:17:19 -0700

    feat: testing in main.cpp and gitignore

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "pitch_detection/pitch_algorithm.h"
+
+using namespace std;
+
+int main() {
+    // Testing
+    int16_t samples[1024] = {0}; 
+    float frequency = detectFrequencyAutoCor(samples, 1024, 44100);
+    cout << "Detected Frequency: " << frequency << endl;
+    return 0;
+}
